5edcabbbf30e05a2847d175007a720cd7debd34d
Nilesh Patil <nileshpatil2514@gmail.com>
2022-05-18 12:57:19 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,24 @@ import { ItemComponent } from './main-container/item/item.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+
+const RoutesConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactUsComponent
+  },
+  {
+    path: 'list',
+    component: ListItemComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,7 +45,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(RoutesConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
